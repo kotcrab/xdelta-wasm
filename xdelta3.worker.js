e6b1e9d95de05a182fdf29f010ed53f66bdfd2dd
kotcrab <4594081+kotcrab@users.noreply.github.com>
2025-07-08 22:33:50 +0200

    Updates

--- xdelta3.worker.js
+++ xdelta3.worker.js
@@ -1,44 +1,59 @@
-// eslint-disable-next-line no-restricted-globals
-self.importScripts("./xdelta3.js")
+import createXdelta3Module from './xdelta3.js'
+import {LRUCache} from './lru-cache.js'
+
+const bufferSize = 64 * 1024
 
 const state = {
   sourceFile: undefined,
+  sourceCache: undefined,
   patchFile: undefined,
+}
+
+const lruCacheOptions = {
+  max: 64
 }
 
 let module = undefined
 let errorMessage = undefined
 
-// eslint-disable-next-line no-undef,no-unused-vars
+// eslint-disable-next-line no-undef
 const reader = new FileReaderSync()
 
-// eslint-disable-next-line no-unused-vars
 function readSource(buffer, offset, size) {
-  return readFile(state.sourceFile, buffer, offset, size)
+  return readFile(state.sourceFile, buffer, offset, size, state.sourceCache)
 }
 
-// eslint-disable-next-line no-unused-vars
 function readPatch(buffer, offset, size) {
-  return readFile(state.patchFile, buffer, offset, size)
+  return readFile(state.patchFile, buffer, offset, size, null)
 }
 
-function readFile(file, buffer, offset, size) {
+function readFile(file, buffer, offset, size, cache) {
+  if (cache && size === bufferSize) {
+    const cached = cache.get(offset)
+    if (cached) {
+      module.HEAP8.set(cached.data, buffer)
+      return cached.read
+    }
+  }
   const end = Math.min(file.size, offset + size)
   const blob = file.slice(offset, end)
   const read = end - offset
+  // console.log("read: " + file.name + " offset: " + offset + " size: " + size);
   const data = reader.readAsArrayBuffer(blob)
-  module.HEAP8.set(new Uint8Array(data), buffer)
+  const dataArray = new Uint8Array(data)
+  if (cache && size === bufferSize) {
+    cache.set(offset, {read: read, data: dataArray})
+  }
+  module.HEAP8.set(dataArray, buffer)
   return read
 }
 
-// eslint-disable-next-line no-unused-vars
 function outputFile(buffer, size) {
   const dataView = new Uint8Array(module.HEAP8.buffer, buffer, size)
   const data = new Uint8Array(dataView)
   postMessage({final: false, bytes: data})
 }
 
-// eslint-disable-next-line no-unused-vars
 function reportError(buffer) {
   errorMessage = module.UTF8ToString(buffer)
 }
@@ -53,13 +68,17 @@ onmessage = async function (event) {
   }
 
   state.sourceFile = sourceFile
+  state.sourceCache = new LRUCache(lruCacheOptions);
   state.patchFile = patchFile
   try {
     console.log("Loading module")
-    // eslint-disable-next-line no-undef
     module = await createXdelta3Module()
+    module.readPatch = readPatch
+    module.readSource = readSource
+    module.outputFile = outputFile
+    module.reportError = reportError
     console.log("Starting module")
-    const result = module.callMain([])
+    const result = module.callMain([bufferSize.toString()])
     if (result !== 0) {
       postMessage({final: true, error: true, errorCode: result, errorMessage: errorMessage})
     } else {
